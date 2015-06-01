61d9ede4525a9d4818f51a78bc58359ee6d82dd0
unknown <jean.carlos@ES053.ellevo.com.br>
2015-06-01 15:52:06 -0300

    Initial Commit

---
+++ js/keyStrokes.js
@@ -0,0 +1,75 @@
+var EDir = {
+    escape: 13
+	, space: 32
+	, left: 37
+	, up: 38
+	, right: 39
+	, down: 40
+    , w: 87
+    , a: 65
+    , s: 83
+    , d: 68
+}
+
+function KeyInput(){}
+
+KeyInput.lastCursor = 1;
+KeyInput.lastDirection = EDir.right;
+
+KeyInput.startListening = function()
+{
+    window.onkeydown = function(e)
+    {
+        var keyCode = e.which ? e.which : e.keyCode;
+        if (KeyInput.lastDirection === 1 || KeyInput.lastDirection === -1)
+        {
+            switch (keyCode)
+            {
+                case EDir.up:
+                case EDir.w:
+                    KeyInput.lastCursor = -20;
+                    break;
+                case EDir.down:
+                case EDir.s:
+                    KeyInput.lastCursor = 20;
+                    break;
+                default:
+                    return;
+            }
+        }
+        else (KeyInput.lastDirection === 20 || KeyInput.lastDirection === -20)
+        {
+            switch (keyCode)
+            {
+                case EDir.right:
+                case EDir.d:
+                    KeyInput.lastCursor = 1;
+                    break;
+                case EDir.left:
+                case EDir.a:
+                    KeyInput.lastCursor = -1;
+                    break;
+                default:
+                    return;
+            }
+        }
+	}
+}
+
+KeyInput.getLastCursor = function(id)
+{
+	var dir = KeyInput.lastCursor;
+
+	if (id % 20 === 0 && dir === 1)
+		dir = -19;
+	else if ((id -1) % 20 === 0 && dir === -1)
+		dir = 19;
+	else if (id <= 20 && dir === -20)
+		dir = 380;
+	else if (id > 380 && dir === 20)
+		dir = -380;
+
+	KeyInput.lastDirection = KeyInput.lastCursor;
+
+	return id + dir;
+}
---
+++ js/snake.js
@@ -0,0 +1,224 @@
+function getBaseSize() {
+    return Math.round((window.innerHeight < window.innerWidth ? window.innerHeight : window.innerWidth) / 30);
+}
+
+function getRandomColor() {
+	var colors = ["purple", "red", "green", "yellow", "blue", "gray", "brown", "cyan", "orange", "pink"];
+	return colors[Math.floor(Math.random() * colors.length)];
+}
+
+Array.prototype.findById = function(id, deleteRecord) {
+	for (var i = 0; i < this.length; i++) {
+		if (this[i].id === id) {
+			if (deleteRecord) {
+				this.splice(i, 1);
+            }
+            
+			return true;
+		}
+	}
+	return false;
+}
+
+window.onresize = function () {
+    buildArena(false);
+}
+
+function buildArena(firstTime) {
+    var divID = 0, objDiv;
+    var base = getBaseSize();
+    var arena = document.getElementById("Arena");
+
+    var baseSize = base + "px";
+    arena.style.width = (base * 20) + "px";
+    arena.style.height = (base * 20) + "px";
+
+    for (var y = 1; y <= 20 ; y++) {
+        for (var x = 1; x <= 20 ; x++) {
+            if (firstTime) {
+                objDiv = document.createElement("div");
+                objDiv.id = ++divID;
+            } else {
+                objDiv = document.getElementById(++divID);
+            }
+
+            objDiv.style.height = baseSize;
+            objDiv.style.width = baseSize;
+            objDiv.className = "ArenaBlock";
+
+            document.getElementById("Arena").appendChild(objDiv);
+        }
+    }
+}
+
+function Section(properties) {
+	this.id			= properties.id;
+	this.color		= properties.color;
+	this.hasFruit	= properties.hasFruit;
+	this.div		= document.getElementById(this.id);
+
+	this.draw = function() {
+		this.changeColor(this.color);
+	}
+
+	this.erase = function() {
+		this.changeColor("");
+	}
+	
+	this.changeColor = function(color) {
+		this.div.style.backgroundColor = color;
+	}
+}
+
+function Fruit(properties) {
+    Section.apply(this, arguments);
+}
+
+function Snake(properties) {
+    this.sections = [];
+    this.fruitsEaten = [];
+    this.speed = 170;
+    this.fruitEaten = false;
+    this.lastFruit = 0;
+	this.done = false;
+	this.score = 0;
+}
+
+Snake.prototype = {
+
+	//Sets and prepare all the needed process to start the game
+	init : function() {
+	    buildArena(true);
+		KeyInput.startListening();
+
+		for (var i = 1; i < 3 ; i++) {
+			this.addSection(this.buildSection(i));
+		}
+
+		this.spawnFruit();
+	}
+
+	//Add the section object in the list and draws it
+	,addSection : function(objSection) {
+		this.sections.push(objSection);
+		objSection.draw();
+	}
+
+	//Remove the object section from the list and erases it
+	,removeSection : function(objSection) {
+		this.sections.findById(objSection.id, true);
+		objSection.erase();
+	}
+
+	//Returns a object section
+	,buildSection : function(id, color, hasFruit) {
+		return new Section({
+			id        : id
+			,color    : color ? color : "black" 
+			,hasFruit : (hasFruit || false)
+		});
+	}
+
+	//Intro method to start the game
+	,startGaming : function() {
+		this.init();
+		this.cycle();
+	}
+
+	//Game cycle called every gaming loop	
+	,cycle : function() {
+		this.move();
+		this.setGamingLoop();
+	}
+
+	//Sets the infinite gaming loop, with the dynamic speed
+	,setGamingLoop : function() {
+		if (this.done) return;
+		
+		with (this) {
+			this.t = setTimeout(function(){ cycle(); }, speed);
+		}
+	}
+
+	//Do all the movement logic
+	,move : function() {
+	    var nextMove = KeyInput.getLastCursor(this.getHeadSection().id);
+
+		//Remove the last section of the snake IF there isn't a fruit on the same place
+		if (this.getTailSection().id != this.fruitsEaten[0] || !this.fruitsEaten[0]) {
+			this.removeSection(this.getTailSection());
+        } else {
+			this.fruitsEaten.splice(0, 1);
+        }
+		
+		//If there is any part of the snake body in the way, the player is defeated and the game is reloaded
+		if (this.sections.findById(nextMove, false)) {
+			this.showDefeatedMessage();
+        }
+
+		//Add a new section after the head of the snake, in the informed direction
+		this.addSection(this.buildSection(nextMove));
+
+        //Spawn new fruit only if the last one was eaten
+		if (this.lastFruit == this.getHeadSection().id) {
+		    this.spawnFruit();
+		    this.addSectionToFruitsEaten();
+		    this.addPoints();
+		    this.accelerate();
+		}
+	}
+
+	//Return the head section of the snake
+	,getHeadSection : function() {
+		return this.sections[this.sections.length - 1];
+	}
+
+	//Return the tail section of the snake
+	,getTailSection : function() {
+		return this.sections[0];
+	}
+
+	//Spawn the fruit on the arena when the last one is eaten
+	,spawnFruit : function() {	
+		var fruitID;
+		
+		while (true) {
+		    fruitID = Math.ceil(Math.random() * 400);
+
+		    if (!this.sections.findById(fruitID, false)) {
+		        break;
+            }
+		}
+
+		var fruit = new Fruit({
+		    id: fruitID,
+            color: getRandomColor()
+		});
+
+		fruit.draw();
+		this.lastFruit = fruit.id;
+	}
+
+    //Accelerate the speed of the snake, according to the current speed
+    ,accelerate: function () {
+        this.speed -= this.speed > 120 ? 2 : 1;
+    }
+
+    //Add the current head section of the snake to the fruitsEaten array, so later it can be a whole new section of the snake
+    ,addSectionToFruitsEaten: function () {
+        this.fruitsEaten.push(this.getHeadSection().id);
+    }
+
+    //Add points to the score system
+    ,addPoints: function () {
+        this.score += 10;
+        document.getElementById("Score").innerHTML = "Score: " + this.score;
+    }
+
+    //Shows a message and restart the game
+	,showDefeatedMessage: function() {
+		this.done = true;
+		alert("You lose bro.\nScore: " + this.score);
+		location.reload();
+	}
+}
